42a4a50ba9b218999f8206647350216c043f8b7f
wangkaiwd <1162486176@qq.com>
2019-07-16 11:54:43 +0800

    feat: commit project all file

---
+++ craco.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const WebpackBar = require('webpackbar');
+const { whenDev, whenProd } = require('@craco/craco');
+module.exports = {
+  eslint: {
+    enable: false
+  },
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    },
+    plugins: [
+      new WebpackBar()
+    ],
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContextDemo from './ContextDemo';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <ContextDemo/>
+    </div>
+  );
+};
+
+export default App;
