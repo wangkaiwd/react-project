4bff01fb737b57f2a1ffd62af06f6538debdd1ee
wangkaiwd <1162486176@qq.com>
2019-07-17 22:29:35 +0800

    feat: contextType usage

--- src/ContextDemo.jsx
+++ src/ContextDemo.jsx
@@ -2,19 +2,34 @@ import React, { Component, createContext } from 'react';
 
 const BatteryContext = createContext(undefined);
 const Middle = () => <Leaf/>;
-const Leaf = () => {
-  return (
-    <BatteryContext.consumer>
-      {battery => <h2>battery: {battery}</h2>}
-    </BatteryContext.consumer>
-  );
-};
+// 函数式组件只能通过Consumer以及将函数作为子组件的方式来接收context
+// const Leaf = () => {
+//   return (
+//     <BatteryContext.Consumer>
+//       {battery => <h2>battery: {battery}</h2>}
+//     </BatteryContext.Consumer>
+//   );
+// };
+
+class Leaf extends Component {
+  // 在类组件中可以使用静态属性来接收context,之后就可以直接通过this.context来进行访问
+  static contextType = BatteryContext;
+
+  render () {
+    return (
+      <h2>
+        battery:{this.context}
+      </h2>
+    );
+  }
+}
+
 class ContextDemo extends Component {
   render () {
     return (
-      <BatteryContext.provider value={40}>
+      <BatteryContext.Provider value={40}>
         <Middle/>
-      </BatteryContext.provider>
+      </BatteryContext.Provider>
     );
   }
 }
