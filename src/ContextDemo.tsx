7c9cdd0881685fdd2bb468e3e278d49e3fe2d86b
wangkaiwd <1162486176@qq.com>
2019-07-18 15:00:49 +0800

    feat: contextType use in class component

--- src/ContextDemo.tsx
+++ src/ContextDemo.tsx
@@ -1,9 +1,11 @@
 import React, { Component, createContext } from 'react';
 
 // context: 地区偏好，UI主题这些数据可以通过context来管理，极大的减少了传递属性的繁琐性
+// 在正常使用中，尽可能不要使用多个context,会导致代码不易维护
 const BatteryContext = createContext(90);
 const StatusContext = createContext('onLine');
-const Middle = () => <Leaf/>;
+const Middle = () => <LeafClass/>;
+// 函数式组件通过Consumer访问函数中的参数来获取到最近的Provider提供的value值
 const Leaf = () => (
   <BatteryContext.Consumer>
     {
@@ -18,15 +20,39 @@ const Leaf = () => (
   </BatteryContext.Consumer>
 );
 
+interface LeafState {
+
+}
+
+interface LeafProps {
+
+}
+
 // 在class组件中通过contextType来访问最近的Provider提供的value值
-// 函数式组件通过Consumer访问函数中的参数来获取到最近的Provider提供的value值
+// Deprecated symbol used, consult docs for better alternative
+class LeafClass extends Component <{}, {}> {
+  static contextType = BatteryContext;
+
+  render () {
+    return (
+      <h2>
+        {this.context}
+      </h2>
+    );
+  }
+}
+
 interface StateProps {
   battery: number,
   status: 'onLine' | 'offLine'
 }
 
+interface PropProps {
+
+}
+
 // reference: https://github.com/typescript-cheatsheets/react-typescript-cheatsheet
-class ContextDemo extends Component<{}, StateProps> {
+class ContextDemo extends Component<PropProps, StateProps> {
   state: StateProps = {
     battery: 60,
     status: 'onLine'
