89fba91cff6fde963b447d95cfc309ec13d6152b
wangkaiwd <1162486176@qq.com>
2019-07-18 15:14:24 +0800

    feat: 使用lazy和suspense 异步加载组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ContextDemo from '@/ContextDemo';
+import React, { lazy, Suspense } from 'react';
+// import ContextDemo from '@/ContextDemo';
+
+const ContextDemo = lazy(() => import(/* webpackChunkName: "contextDemo" */'./ContextDemo'));
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <ContextDemo/>
+      <Suspense fallback={<div>loading...</div>}>
+        <ContextDemo/>
+      </Suspense>
     </div>
   );
 };
