1fdecee178f52b83f45d3fcccb756370bf4f8cd7
wangkaiwd <1162486176@qq.com>
2019-07-20 23:45:32 +0800

    feat: 建立自定义Hooks文件

---
+++ src/CustomHooks.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// 定义如下自定义hooks:
+//  1. 自增长count
+//  2. 计算屏幕宽高尺寸
+const CustomHooks = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default CustomHooks;
