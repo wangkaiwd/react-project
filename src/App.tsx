060a5bae058ae7dc2a204f7418576a65e9c08345
wangkaiwd <1162486176@qq.com>
2019-07-23 15:46:32 +0800

    feat: 添加redux的部分文件

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,10 @@ interface Props {
 
 }
 
-// 一个真实的业务场景：
-//    稍微复杂一些： 可编辑表格，当表格内容编辑的时候，我们要更改原来请求到的数据
-
 const App: React.FC<Props> = () => {
-  const [{ isLoading, data, error }] = useRequest(fetchDemo);
-  const [{ isLoading: isLoading1, data: data1, error: error1 }, changeDoFetch] = useRequest(fetchDemo, {}, 'manual');
-  useEffect(() => {
-    console.log('data1', data1);
-  }, [data1]);
   return (
     <div>
-      <Button size={'small'} type={'primary'} onClick={() => {
-        console.log('click');
-        changeDoFetch();
-      }}>
+      <Button size={'small'} type={'primary'}>
         click
       </Button>
     </div>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,2 +1,5 @@
-const store = {};
+import { createStore } from 'redux';
+import reducer from '@/store/reducer';
+
+const store = createStore(reducer);
 export default store;
